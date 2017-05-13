ff7c774edb41c83ce6e9bf5fecb4e2b8e110c3f2
Leandro Ibarra <lea14_9@hotmail.com>
2017-05-13 16:14:50 -0300

    Se ajustan manejo de headers

--- api/index.php
+++ api/index.php
@@ -18,4 +18,7 @@ require 'routes/route.campanias.php';
 require 'routes/route.causas.php';
 require 'routes/route.login.php';
 
+// http://help.slimframework.com/discussions/problems/760-backbonejs-slim-and-cross-domain-question
+if ("OPTIONS" == $_SERVER['REQUEST_METHOD']) { exit(0); }
+
 $app->run();
