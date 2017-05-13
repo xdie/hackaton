09a0ffdc393554ddcba2e8fb0479b18a32ddb1e4
Braulio <bevangelisti@dynamicmind.com>
2017-05-13 15:34:58 -0300

    update campanias routes

--- api/routes/route.campanias.php
+++ api/routes/route.campanias.php
@@ -1,13 +1,9 @@
 <?php 
-	$app->group('/campanias', function() use($app) {
-	    $app->get('/', function() use($app) {
-			$result = $db->get('campanias'); 
-	        $app->render(200, array(
-	            'code' => 200,
-	            'status' => 'success',
-	            'error' => false,
-	            'data' => $result
-	        ));
+	$app->group('/campanias', function() use($app, $db) {
+	    $app->get('/', function() use($app, $db) {
+			$result['data'] = $db->get('campanias'); 
+	        if($result['data'])
+	        	$result['status'] = 200;
 	    });
 
 	    $app->get('/:idCampania(/)', function($idCampania) use($app) {
