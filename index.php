05fce55640adb3fa849829c8ad9efdeeaa4afafe
Braulio <bevangelisti@dynamicmind.com>
2017-05-13 14:09:55 -0300

    update rutas

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 include 'vendor/autoload.php';
 
+$app = new \Slim\Slim();
+
 require 'api/routes/route.campanias.php';
 
-
-
-$app = new \Slim\Slim();
 $app->get('/hello/:name', function ($name) {
     echo "Hello, " . $name;
 });
+
 $app->run();
